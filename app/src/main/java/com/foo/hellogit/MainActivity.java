059ff2d9ba6dda7da961cdb5246d15021603e52a
freedomu <1072672770@qq.com>
2016-05-26 09:59:45 +0800

    Initial commit

---
+++ app/src/main/java/com/foo/hellogit/MainActivity.java
@@ -0,0 +1,25 @@
+package com.foo.hellogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_main).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // toast
+                Toast.makeText(MainActivity.this, "haha", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+
+    }
+}
